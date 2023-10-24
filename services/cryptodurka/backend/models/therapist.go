371f06f9d0f031f85cd05966a449ca8c476a5aae
gusyaross <nikdoctor2@gmail.com>
2023-10-24 06:37:19 +0600

    remove golang files

--- services/cryptodurka/backend/models/therapist.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/google/uuid"
-
-type Therapist struct {
-	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	Username string `gorm:"uniqueIndex;type:varchar(100);not null"`
-	Password string `gorm:"type:varchar(100);not null"`
-}
